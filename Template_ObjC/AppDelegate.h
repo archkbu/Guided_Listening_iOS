fbb4d2ff20e826c784c3946a2dcd0c2dabcfb8fe
Kwok Ho FUNG <fungkwokhodave@outlook.com>
2015-10-07 10:33:38 +0800

    Initial Commit

---
+++ Template_ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Template_ObjC
+//
+//  Created by Kwok Ho FUNG on 7/10/2015.
+//  Copyright © 2015 ULIP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Template_ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Template_ObjC
+//
+//  Created by Kwok Ho FUNG on 7/10/2015.
+//  Copyright © 2015 ULIP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
